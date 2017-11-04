0946eec851dc8e9b94febb9c630ba86049e711e8
ciolicb <cbcioli@fullsail.edu>
2017-11-04 10:42:38 -0400

    ADD - Initial Commit of current files

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public searchBySelector = "";
+  public showDoctorText = false;
+  public dateSelector = false;
+  public myDate;
   constructor(public navCtrl: NavController) {
 
+  }
+
+  showControls() {
+    if(this.searchBySelector = "Name") {
+      this.showDoctorText = true;
+    }
+  }
+
+  showDatePicker() {
+    this.dateSelector = true;
   }
 
 }
