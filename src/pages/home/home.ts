d8b8ab2977eed2e609be6a95f6ffbc01d2720da8
CioliCB <cbcioli@fullsail.edu>
2017-11-05 13:20:46 -0500

    ADD - Added functionality for different Search By selections

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,21 +8,61 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   public searchBySelector = "";
-  public showDoctorText = false;
-  public dateSelector = false;
+  public showTextInput = false;
+  public showDateSelector = false;
+  public showHospitalList = false;
+  public showDoctorList = false;
+  public showSpecialtyList = false;
+  public showApptButton = false;
   public myDate;
+  public minDate = new Date();
+  public maxDate = new Date(this.minDate.getFullYear() + 1, 11);
+
   constructor(public navCtrl: NavController) {
 
   }
 
   showControls() {
-    if(this.searchBySelector = "Name") {
-      this.showDoctorText = true;
+    if(this.searchBySelector === "Name") {
+      this.showTextInput = true;
+      this.showDoctorList = true;
+      this.showDateSelector = false;
+      this.showHospitalList = false;
+      this.showSpecialtyList = false;
+      this.showApptButton = false;
+    }
+    else if (this.searchBySelector === "Specialty") {
+      this.showTextInput = true;
+      this.showSpecialtyList = true;
+      this.showDateSelector = false;
+      this.showHospitalList = false;
+      this.showDoctorList = false;
+      this.showApptButton = false;
+    }
+    else if (this.searchBySelector === "Hospital") {
+      this.showHospitalList = true;
+      this.showTextInput = true;
+      this.showDateSelector = false;
+      this.showDoctorList = false;
+      this.showSpecialtyList = false;
+      this.showApptButton = false;
     }
   }
 
   showDatePicker() {
-    this.dateSelector = true;
+    this.showDateSelector = true;
+  }
+
+  showHospitalDoctorList() {
+    this.showDoctorList = true;
+  }
+
+  showHospitalSpecialtyList() {
+    this.showSpecialtyList = true;
+  }
+
+  showButton() {
+    this.showApptButton = true;
   }
 
 }
